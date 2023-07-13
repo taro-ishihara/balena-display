9bcd21d022110e2a055124325c0e6d605c5498c4
Taro <taro.ishihara.mail@gmail.com>
2023-07-13 13:24:35 -0700

    fixed memory issue

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -8,7 +8,7 @@ sed -i -e 's/console/anybody/g' /etc/X11/Xwrapper.config
 dpkg-reconfigure xserver-xorg-legacy
 
 ## Disable CPU Throttling
-echo 'performance' > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor 
+echo 'performance' > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
 ## Pass Environment Values to New Session
 environment=$(env | grep -v -w '_' | awk -F= '{ st = index($0,"=");print substr($1,0,st) ","}' | tr -d "\n")
--- src/display.ts
+++ src/display.ts
@@ -5,6 +5,8 @@ import path from 'path'
 import os from 'os'
 
 interface Display {
+  name: string
+  number: number
   w: number
   h: number
   p: number
@@ -12,16 +14,23 @@ interface Display {
 
 const defaultRemoteDebuggingPort = 9222
 
-const formatDisplay = (display: string): Display | undefined => {
+const formatDisplay = (display: string, index: number): Display | undefined => {
   const sizeRegex = /(\d+)x(\d+)/
   const positionRegex = /\+(\d+)/
   const sizeMatch = display.match(sizeRegex)
   const positionMatch = display.match(positionRegex)
   if (sizeMatch && positionMatch) {
+    const number = index + 1
     const width = parseInt(sizeMatch[1])
     const height = parseInt(sizeMatch[2])
     const position = parseInt(positionMatch[1])
-    return { w: width, h: height, p: position }
+    return {
+      name: `display${number}`,
+      number: number,
+      w: width,
+      h: height,
+      p: position,
+    }
   } else {
     return undefined
   }
@@ -32,13 +41,12 @@ const displayList = ((): Array<Display> => {
   const display_2 = process.env.DISPLAY_2 || ''
 
   return [display_1, display_2]
-    .map((value) => formatDisplay(value))
+    .map((value, index) => formatDisplay(value, index))
     .filter((value) => value !== undefined) as Display[]
 })()
 
 const createFlags = (
   display: Display,
-  remoteDebuggingPort,
   debug: 'local' | 'remote' | undefined,
 ) => {
   const positionFlags = [
@@ -49,6 +57,8 @@ const createFlags = (
     '--check-for-update-interval=31536000',
     '--disable-background-mode',
     '--autoplay-policy=no-user-gesture-required',
+    '--disable-features=TranslateUI',
+    '--disable-dev-shm-usage',
   ]
   const corsFlags = [
     '--disable-web-security',
@@ -69,41 +79,42 @@ const createFlags = (
         ...corsFlags,
         '--headless',
         '--remote-debugging-address=0.0.0.0',
-        `--remote-debugging-port=${remoteDebuggingPort}`,
+        `--remote-debugging-port=${
+          defaultRemoteDebuggingPort + display.number - 1
+        }`,
       ]
     default:
-      return [
-        ...positionFlags,
-        ...baseFlags,
-        ...corsFlags,
-        '--kiosk',
-      ]
+      return [...positionFlags, ...baseFlags, ...corsFlags, '--kiosk']
   }
 }
 
 const launchDisplay = async (debug?: 'local' | 'remote') => {
   killAll()
-  const promises = displayList.map(async (display, index) => {
-    // if you only set LAUNCH_URL_1 and start as dual display, the display will set as mirror mode
+  const promises = displayList.map(async (display) => {
+    // if you only set LAUNCH_URL_1 and start as dual display, the display will set as mirror mode.
     const url =
-      process.env[`LAUNCH_URL_${index + 1}`] ||
+      process.env[`LAUNCH_URL_${display.number}`] ||
       process.env.LAUNCH_URL_1 ||
       'https://example.com'
     const startingUrl = debug ? url : `--app=${url}`
-    const remoteDebuggingPort = defaultRemoteDebuggingPort + index
-    const flags = createFlags(display, remoteDebuggingPort, debug)
-    await launch({
+    const flags = createFlags(display, debug)
+    const remoteDebuggingPortFlag = flags.find((flag) =>
+      flag.startsWith('--remote-debugging-port'),
+    )
+    const chromiumInstance = await launch({
       startingUrl: startingUrl,
-      port: debug === 'remote' ? remoteDebuggingPort : undefined,
+      port: remoteDebuggingPortFlag
+        ? parseInt(remoteDebuggingPortFlag.split('=')[1])
+        : undefined,
       chromeFlags: flags,
-      userDataDir: `/chromium/display${index + 1}`,
+      userDataDir: `/chromium/${display.name}`,
       ignoreDefaultFlags: true,
     })
-    console.log(`${url} launched with flags: ${flags}`)
-    debug === 'remote'
-      ? console.log(`Remote debugging port: ${remoteDebuggingPort}`)
+    console.log(`URL: ${url} launched with flags: ${flags}`)
+    remoteDebuggingPortFlag
+      ? console.log(`Remote debugging port: ${chromiumInstance.port}`)
       : null
-    return remoteDebuggingPort
+    return chromiumInstance.port
   })
   return await Promise.all(promises)
 }
