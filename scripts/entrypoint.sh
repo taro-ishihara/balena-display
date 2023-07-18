6565290e966fe2ce45fdef6c1ce2f789da761f61
Taro <taro.ishihara.mail@gmail.com>
2023-07-18 13:00:40 -0700

    Show cursor

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,10 +10,19 @@ dpkg-reconfigure xserver-xorg-legacy
 ## Disable CPU Throttling
 echo 'performance' > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
-## Pass Environment Values to New Session
+## Show Cursor
+if [[ -n $SHOW_CURSOR ]] && [[ $SHOW_CURSOR -eq 1 ]]; then
+  export CURSOR=''
+  echo "Enabling cursor"
+else
+  export CURSOR='-- -nocursor'
+  echo "Disabling cursor"
+fi
+
+## Pass Environment Variables to New Session
 environment=$(env | grep -v -w '_' | awk -F= '{ st = index($0,"=");print substr($1,0,st) ","}' | tr -d "\n")
 environment="${environment::-1}"
 
 ## Change User to "chromium" and startx with start_server.sh
-su -w $environment -c "export DISPLAY=:0 && startx /app/scripts/start_server.sh -- -nocursor" - chromium
+su -w $environment -c "export DISPLAY=:0 && startx /app/scripts/start_server.sh $CURSOR" - chromium
 balena-idle
